23bacef4eb23d63b766af6d499e858a05684942a
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-01 23:30:10 +0530

    Initial commit

---
+++ StandardProgram/src/com/nit/basicsofjava/ForLoopDemo.java
@@ -0,0 +1,14 @@
+package com.nit.basicsofjava;
+
+public class ForLoopDemo {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i<10 ; i++){
+			
+			System.out.println("Im the Boss");
+			
+		}
+	}
+
+}
