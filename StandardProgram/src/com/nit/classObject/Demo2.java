900d46ce10bf4a007b1760e221d74a5ef7bd430b
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-19 15:54:24 +0530

    initial commit

--- StandardProgram/src/com/nit/classObject/Demo2.java
+++ StandardProgram/src/com/nit/classObject/Demo2.java
@@ -5,9 +5,23 @@ public class Demo2 {
 	public static void main(String[] args) {
 
 		Car1 c1 = new Car1();
-		
-		c1.initialization("Honda", "Black", 18.0);
+
+		c1.initialization("Honda", "Red", 18.0);
 		c1.displayDetails();
+		System.out.println("\n");
+
+		Car1 c2 = new Car1();
+
+		c2.initialization("Toyota", "Silver", 15.8);
+		c2.displayDetails();
+		System.out.println("\n");
+
+		Car1 c3 = new Car1();
+
+		c3.initialization("BMW", "Black", 17.4);
+		c3.displayDetails();
+		System.out.println("\n");
+
 	}
 
 }
@@ -17,13 +31,14 @@ class Car1 {
 	public String company;
 	public String color;
 	public double milage;
-	
-	public void initialization(String comp, String clr, double mil) {
-		
-		company = comp;
-		color = clr;
-		milage = mil;
-		
+
+	public void initialization(String company, String color, double milage) {
+
+		this.company = company;
+		this.color = color;
+		this.milage = milage;
+		// use this. when variable and method have same name like this company and
+		// company.
 	}
 
 	public void displayDetails() {
---
+++ StandardProgram/src/com/nit/classObject/Demo4.java
@@ -0,0 +1,27 @@
+package com.nit.classObject;
+
+class Demo4 {
+	public static void main(String[] args) {
+		Car2 c1 = new Car2("Whyyy");
+		Car2 c2 = new Car2();
+		c1.jump();
+	}
+}
+
+class Car2 {
+	
+	public Car2() {
+		
+		System.out.println("Byeee");
+	}
+	
+	public Car2(String s) {
+		System.out.println("Hello");
+		System.out.println(s);
+	}
+
+	public void jump() {
+		System.out.println("Jump Bitchhh");
+	}
+}
+
