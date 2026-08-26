879308f106cc156f366a30ddc3ce10bfb9013b19
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-26 16:06:39 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/classObject/Demo6.java
@@ -0,0 +1,14 @@
+package com.nit.classObject;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		System.out.println(C.k);
+	
+	}
+}   
+class C {
+
+		public static int k = 10;
+}
+ 
---
+++ StandardProgram/src/com/nit/encapsulationPrograms/Qsn01.java
@@ -0,0 +1,81 @@
+package com.nit.encapsulationPrograms;
+
+import java.util.Scanner;
+
+public class Qsn01 {
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("AccNo & Balance : ");
+	String accountNumber = sc.nextLine();
+	double balance = sc.nextDouble();
+	System.out.println("Enter amount to deposit");
+	double deposit = sc.nextDouble();
+	
+}
+
+	
+}
+class BankAccount {
+
+	private String accountNumber;
+	private double balance;
+
+	public BankAccount(String accountNumber, double balance) {
+
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+	public void deposit(double amount) {
+
+		if (amount > 0) {
+
+			balance += amount;
+			System.out.println("Amount of " + amount + "is been deposited...!");
+		} else {
+			System.out.println("Invalid deposit amount.");
+		}
+	}
+
+	public void withdraw(double amount) {
+		if (amount > 0 && amount <= balance) {
+
+			balance -= amount;
+			System.out.println("Withdrawl of " + amount + "is dome successfully...!");
+		} else {
+			System.out.println("Insufficient Balance ");
+		}
+
+	}
+
+	public double getBalance() {
+
+		return balance;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+}
+
+class MobileApp extends BankAccount {
+
+	public MobileApp(String accountNumber, double balance) {
+		
+		super(accountNumber, balance);
+
+	}
+	public void withdraw(double amount) {
+		
+		if(amount>25000) {
+			System.out.println("Daily limit is exceeded...!");
+		}
+		else {
+			super.withdraw(amount);
+		}
+		
+	}
+
+} 
