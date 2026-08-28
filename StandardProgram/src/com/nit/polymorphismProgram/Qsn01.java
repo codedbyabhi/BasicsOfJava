162ee62097ddf39e691cf73881f048b32acca2c2
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-28 15:28:43 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/polymorphismProgram/Qsn01.java
@@ -0,0 +1,24 @@
+package com.nit.polymorphismProgram;
+
+public class Qsn01 {
+public static void main(String[] args) {
+	
+	SalaryCalculator sc = new SalaryCalculator();
+	System.out.println(sc.calculateSalary(50000));
+	System.out.println(sc.calculateSalary(20,500));
+	System.out.println(sc.calculateSalary(30000.0,5000.0));
+ 	}
+}
+class SalaryCalculator{
+	
+	public double calculateSalary(double monthlySalary) {
+		return monthlySalary;
+	}
+	public double calculateSalary(int hours, double ratePerHour) {
+		return hours*ratePerHour;
+	}
+	public double calculateSalary(double fixedAmount, double bonus) {
+		return fixedAmount+bonus;
+	}
+	
+}
