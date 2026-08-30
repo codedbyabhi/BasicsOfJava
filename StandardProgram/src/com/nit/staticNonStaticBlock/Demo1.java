242f6d69a32f2c956d97f95d830a25b10210ae94
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-30 23:56:55 +0530

    initial commit

--- StandardProgram/src/com/nit/staticNonStaticBlock/Demo1.java
+++ StandardProgram/src/com/nit/staticNonStaticBlock/Demo1.java
@@ -1,14 +1,25 @@
 package com.nit.staticNonStaticBlock;
 
 public class Demo1 {
+	static {
+		System.out.println("Moon");
+	}
+	public Demo1() {
+		System.out.println("Sun");
+	}
 	public static void main(String[] args) {
-		B obj = new B();
-		B obj1 = obj;
-		obj.k = 200;
-		System.out.println(obj1.k);
+		
+		System.out.println("Bye");
+		Demo1 d = new Demo1();
+		B d1 = new B();
+	}
+	{
+		System.out.println("Hello");
 	}
 }
 
 class B {
-	public int k = 100;
+	public B() {
+		System.out.println("Good");
+	}
 }
