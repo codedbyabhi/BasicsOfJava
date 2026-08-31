6ce93cfc53ed380bc59570c4536c919100bd49a9
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-08-31 15:19:44 +0530

    initial commit

--- StandardProgram/src/com/nit/templteMethodPattern/Main01.java
+++ StandardProgram/src/com/nit/templteMethodPattern/Main01.java
@@ -1,17 +1,27 @@
 package com.nit.templteMethodPattern;
 
 public class Main01 {
+	public static void main(String[] args) {
 
+//		ATM op1 = new Withdraw();
+//		op1.process();
+		ATM op2 = new Deposit();
+		op2.process();
+//		ATM op3 = new CheckBalance();
+//		op3.process();
+
+	}
 }
 
 abstract class ATM {
-	//template method 
+	// template method
 	final public void process() {
 		insertCard();
 		enterPIN();
 		doTransaction();
 		printRecept();
 	}
+
 	public void insertCard() {
 		System.out.println("Card is inserted...");
 	}
@@ -25,4 +35,30 @@ abstract class ATM {
 	public void printRecept() {
 		System.out.println("Recept is printed...");
 	}
+}
+
+class Withdraw extends ATM {
+
+	public void doTransaction() {
+
+		System.out.println("Amount is withdrawn successfully...");
+
+	}
+
+}
+
+class Deposit extends ATM {
+	public void doTransaction() {
+
+		System.out.println("Amount is deposited successfully...");
+	}
+
+}
+
+class CheckBalance extends ATM {
+
+	public void doTransaction() {
+		System.out.println("Check the Available balance successfully...");
+	}
+
 }
