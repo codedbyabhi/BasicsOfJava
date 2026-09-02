23efd1b9b91715bf144dec2b84dca93872018633
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-02 14:14:18 +0530

    initial commit

--- StandardProgram/src/com/nit/inheritanePrograms/LabTask01.java
+++ StandardProgram/src/com/nit/inheritanePrograms/LabTask01.java
@@ -1,5 +1,35 @@
 package com.nit.inheritanePrograms;
 
-public class LabTask01 {
+import java.util.Scanner;
 
+public class LabTask01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		String status = sc.nextLine();
+		CheckUp c = new CheckUp(name, status);
+		
+		c.display();
+	}
+
+}
+class Patient{
+	 String name;
+
+	public Patient(String name) {
+		this.name = name;
+	}
+}
+class CheckUp extends Patient{
+	 
+	String status;
+	
+	 public CheckUp(String name, String status) {
+		super(name);
+		this.status=status;
+		
+	}
+	 void display() {
+		 System.out.println("Patient: " + name + " | Checkup: " + status);
+	 }
 }
