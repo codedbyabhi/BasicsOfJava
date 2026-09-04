d16d97705945a5e1f068bdcb6a35f15e3e644512
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-04 15:39:08 +0530

    initial commit

--- StandardProgram/src/com/nit/arrayPrograms/Qsn10.java
+++ StandardProgram/src/com/nit/arrayPrograms/Qsn10.java
@@ -3,9 +3,9 @@ package com.nit.arrayPrograms;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Qsn09 {
+public class Qsn10 {
 	public static void main(String[] args) {
-
+	
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter array size : ");
 		int size = sc.nextInt();
@@ -16,10 +16,15 @@ public class Qsn09 {
 
 			a[i] = sc.nextInt();
 		}
-		int [] b = new int[a.length];
-		for(int i = 0;i<=a.length-1;i++) {
-			b[i] = a[(a.length-1)-i];
+		int x = 0;
+		int y = a.length-1;
+		while(x<y) {
+			int temp = a[x];
+			a[x]=a[y];
+			a[y]=temp;
+			x++;
+			y--;
 		}
-		System.out.println("Result is = "+Arrays.toString(b));
+		System.out.println("Result = "+Arrays.toString(a));
 	}
 }
