2f97904c14b91a4d03c08e6bb58af972ae9231aa
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-04 14:39:55 +0530

    initial commit

--- StandardProgram/src/com/nit/arrayPrograms/Qsn06.java
+++ StandardProgram/src/com/nit/arrayPrograms/Qsn06.java
@@ -2,7 +2,7 @@ package com.nit.arrayPrograms;
 
 import java.util.Scanner;
 
-public class Qsn05 {
+public class Qsn06 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -14,15 +14,14 @@ public class Qsn05 {
 		for (int i = 0; i <= a.length - 1; i++) {
 
 			a[i] = sc.nextInt();
-		}
-		int largest = a[0];
+		}int smallest = a[0];
 		for (int e : a) {
 			
-			if (e > largest) {
-				largest = e;
+			if (e < smallest) {
+				smallest = e;
 			}
 
 		}
-		System.out.println("The largest element is " + largest);
+		System.out.println("The largest element is " + smallest);
 	}
 }
