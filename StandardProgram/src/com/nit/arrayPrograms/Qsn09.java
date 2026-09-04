6d1a024a1c477641c3a591510429ba14d00768bc
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-04 15:28:16 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/arrayPrograms/Qsn09.java
@@ -0,0 +1,26 @@
+package com.nit.arrayPrograms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Qsn09 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size : ");
+		int size = sc.nextInt();
+		System.out.println("Enter array element one by one : ");
+		int[] a = new int[size];
+
+		for (int i = 0; i <= a.length - 1; i++) {
+
+			a[i] = sc.nextInt();
+		}
+		int [] b = new int[a.length];
+		for(int i = 0;i<=a.length-1;i++) {
+			b[i] = a[(a.length-1)-i];
+		}
+		System.out.println("Result is = "+Arrays.toString(b));
+	}
+
+}
