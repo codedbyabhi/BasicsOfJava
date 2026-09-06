cd61d5bdf949fda8355f5201b4ff92da9ef30c86
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-07 00:21:26 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/arrayPrograms/ArrayDemo2.java
@@ -0,0 +1,13 @@
+package com.nit.arrayPrograms;
+
+public class ArrayDemo2 {
+	public static void main(String[] args) {
+		
+		int [] a = {10,20,30,40,50};
+		
+		for(int e : a) {
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ StandardProgram/src/com/nit/arrayPrograms/ArrayDemo3.java
@@ -0,0 +1,12 @@
+package com.nit.arrayPrograms;
+
+import java.util.Arrays;
+
+public class ArrayDemo3 {
+public static void main(String[] args) {
+	int [] a = {10,20,30,40,50};
+	
+	System.out.println(Arrays.toString(a));
+	System.out.println(a.length);
+}
+}
