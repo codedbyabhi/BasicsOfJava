2736a568259aa3b707e3b3aac6880091e5ef59e0
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-08 14:57:10 +0530

    initial commit

--- StandardProgram/src/com/nit/arrayPrograms/ArrayLabTask6.java
+++ StandardProgram/src/com/nit/arrayPrograms/ArrayLabTask6.java
@@ -3,42 +3,34 @@ package com.nit.arrayPrograms;
 import java.util.Scanner;
 
 public class ArrayLabTask6 {
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int a[]=new int [size];
-        for (int i=0;i<a.length;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        System.out.print("Original Array: ");
-        for (int i=0;i<a.length;i++)
-        {
-            System.out.print(a[i]+" ");
-        }
-       int index=0;
-       for (int i=0;i<a.length;i++)
-       {
-        if(a[i]!=0)
-         {
-            a[index]=a[i];
-            index++;
-          }
-       }
-       while (index<a.length)
-       {
-        a[index]=0;
-        index++;
-       }
-       System.out.println();
-       System.out.print("Array After Moving Zeros to End: ");
-        for( int i=0;i<a.length;i++)
-        {
-            System.out.print(a[i]+" ");
-        }
-        
-        
-    }
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int a[] = new int[size];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.print("Original Array: ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		int index = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != 0) {
+				a[index] = a[i];
+				index++;
+			}
+		}
+		while (index < a.length) {
+			a[index] = 0;
+			index++;
+		}
+		System.out.println();
+		System.out.print("Array After Moving Zeros to End: ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+
+	}
 }
---
+++ StandardProgram/src/com/nit/arrayPrograms/Qsn20.java
@@ -0,0 +1,37 @@
+package com.nit.arrayPrograms;
+
+import java.util.Scanner;
+
+public class Qsn20 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] a = new int[size];
+
+		for (int i = 0; i <= a.length - 1; i++) {
+			a[i] = sc.nextInt();
+		}
+		boolean repeated = false;
+		for (int i = 0; i <= a.length - 1; i++) {
+
+			boolean isPreviousPresent = false;
+			for (int j = 0; j <= a.length - 1; j++) {
+				if (a[j] == a[i]) {
+
+					isPreviousPresent = true;
+					break;
+				}
+
+			}
+			if (isPreviousPresent == true) {
+				repeated = true;
+				System.out.println("First repeated element : " + a[i]);
+				break;
+			}
+			if (repeated == false) {
+				System.out.println("No arrays element are repeated");
+			}
+		}
+
+	}
+}
