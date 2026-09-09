4a0ad9a3c65bdcc6aa6e149f675dc71a6667430c
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-09 14:53:31 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/arrayPrograms/Qsn26.java
@@ -0,0 +1,23 @@
+package com.nit.arrayPrograms;
+
+import java.util.Scanner;
+
+public class Qsn26 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size : ");
+		int size = sc.nextInt();
+		System.out.println("Enter array element one by one : ");
+		int[] a = new int[size];
+
+		for (int i = 0; i <= a.length - 1; i++) {
+
+			a[i] = sc.nextInt();
+		}
+		int x = 0;
+		for (int i = 0; i <= a.length - 1; i++) {
+			if()
+
+		}
+	}
+}
