acb65278020d6daee84f763f695739e2bd12b3e2
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-14 23:27:42 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/basicsofjava/RelationalOperator2.java
@@ -0,0 +1,22 @@
+package com.nit.basicsofjava;
+
+import java.util.Scanner;
+
+public class RelationalOperator2 {
+	
+	    public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in);
+
+	       int numOfDays = sc.nextInt();
+	       int currentBooking = sc.nextInt(); 
+
+	       if(numOfDays<=2&&currentBooking<6){
+	        System.out.println("Tatkal Allowed");
+	       }
+	       else{
+	        System.out.println("Not Allowed");
+	       }
+	    }
+	}
+
+}
