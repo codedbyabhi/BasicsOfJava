402761c5369f517905832b3de353abcb3d09454d
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-15 15:35:50 +0530

    ic

--- StandardProgram/src/com/nit/twoDArrayProgram/Qsn4.java
+++ StandardProgram/src/com/nit/twoDArrayProgram/Qsn4.java
@@ -6,16 +6,29 @@ public class Qsn4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter outer array size : ");
-		int size1 = sc.nextInt();
-		int[][] a = new int[size1][];
+		int outterArray = sc.nextInt();
+		int[][] a = new int[outterArray][];
 		System.out.println("Enter 2D elements One by One : ");
-		for(int i =0;i<=a.length-1;i++) {
+		for (int i = 0; i <= a.length - 1; i++) {
 			
-			for(int j =0;j<=a[i].length-1;j++) {
-				a[i][j]=sc.nextInt();
+			System.out.println("Enter "+(i+1)+" inner array size");
+			int innerArray = sc.nextInt();
+			a[i]= new int [innerArray];
+			System.out.println("Enter "+(i+1)+" inner array element one by one : ");
+			for (int j = 0; j <= a[i].length - 1; j++) {
+				a[i][j] = sc.nextInt();
 			}
-			
+
 		}
+		System.out.println("Given two directional jagged array is : ");
+		for(int i =0; i<=a.length-1;i++) {
+			
+			for(int j = 0; j<=a[i].length-1;j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
