2ec798c34b63576d55895a450423d50f5f263086
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-15 15:50:08 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/twoDArrayProgram/Qsn5.java
@@ -0,0 +1,33 @@
+package com.nit.twoDArrayProgram;
+
+import java.util.Scanner;
+
+public class Qsn5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of Rows : ");
+		int rows = sc.nextInt();
+		System.out.println("Enter number of Columns : ");
+		int column = sc.nextInt();
+
+		int[][] a = new int[rows][column];
+
+		System.out.println("Enter the Matrix : ");
+		for (int i = 0; i <= a.length - 1; i++) {
+
+			for (int j = 0; j <= a[i].length - 1; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Give the Matrix : ");
+		for (int i = 0; i <= a.length - 1; i++) {
+
+			for (int j = 0; j <= a[i].length - 1; j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
