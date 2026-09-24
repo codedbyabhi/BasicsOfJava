ef3744c90359360118b047f71526f4125869126d
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-24 15:57:27 +0530

    initial commit

--- StandardProgram/src/com/nit/arrayPrograms/BookManage.java
+++ StandardProgram/src/com/nit/arrayPrograms/BookManage.java
@@ -3,30 +3,59 @@ package com.nit.arrayPrograms;
 import java.util.Scanner;
 
 public class BookManage {
+	static Book a[];
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of Books :");
 		int size = sc.nextInt();
-		Book[] a = new Book[size];
+		a = new Book[size];
 
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < a.length; i++) {
 
-			System.out.println("Enter details of book : " + (i + 1));
-			System.out.println("Enter Auther name : ");
-			String authorName = sc.nextLine();
-			System.out.println("Enter tital : ");
-			String tital = sc.nextLine();
-			System.out.println("Enter Price  : ");
-			double price = sc.nextDouble();
-			System.out.println("Enter Published Year : ");
-			int publishedYear = sc.nextInt();
-			
-			a[i]= new Book(authorName, tital, price, publishedYear);
-			
+			System.out.println("Enter AutherName, Book tital, Price, Published Year of book : " + (i + 1));
+			String authorName = IO.readln();
+			String tital = IO.readln();
+			double price = Double.parseDouble(IO.readln());
+			int publishedYear = Integer.parseInt(IO.readln());
+
+			a[i] = new Book(authorName, tital, price, publishedYear);
 		}
-
 	}
+//		System.out.println("Available Books are : ");
+//		for (Book b : a) {
+//			b.displayDetails();
+//		}
+//
+//	}
 
+	public static void BooksByAuthor(String authorName) {
+		for (Book b : a) {
+
+			if (authorName.equalsIgnoreCase(b.authorName)) {
+				b.displayDetails();
+				System.out.println("========================");
+			}
+		}
+	}
+	public static void BooksAfterCertainYear(int year) {
+		for (Book b : a) {
+
+			if (b.publishedYear>year) {
+				b.displayDetails();
+				System.out.println("========================");
+			}
+		}
+	}
+	public static void calculateAvgPrice() {
+		for (Book b : a) {
+
+			if () {
+				b.displayDetails();
+				System.out.println("========================");
+			}
+		}
+	}
 }
 
 class Book {
@@ -44,10 +73,10 @@ class Book {
 
 	public void displayDetails() {
 
-		System.out.println("Title :" + tital);
-		System.out.println("Author Name :" + authorName);
-		System.out.println("Price :" + price);
-		System.out.println("Published year :" + publishedYear);
+		System.out.println("Title : " + tital);
+		System.out.println("Author Name : " + authorName);
+		System.out.println("Price : " + price);
+		System.out.println("Published year : " + publishedYear);
 	}
 
 }
