eea92530d1b36759c8ee72ee0ca649fdebfbad44
Abhijeet Pinjarkar <abhijeetpinjarkar67@gmail.com>
2026-09-26 21:32:05 +0530

    initial commit

---
+++ StandardProgram/src/com/nit/stringProgram/AccentureQsn01.java
@@ -0,0 +1,35 @@
+package com.nit.stringProgram;
+
+import java.util.Scanner;
+
+public class AccentureQsn01 
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String result = "";
+		for(int i = 0;i<=s.length()-1;i++)
+		{
+			if(!result.contains(s.charAt(i)+""))
+			{
+				int count = 0;
+				for(int j = 0;j<=s.length()-1;j++)
+				{
+					if(s.charAt(i)==s.charAt(j))
+					{
+						count++;
+					}
+				}
+				if(count==1)
+				{
+					System.out.println("First non-repeated : "+s.charAt(i));
+					break;
+				}
+			}
+		}
+		
+		
+	}
+	
+}
+
